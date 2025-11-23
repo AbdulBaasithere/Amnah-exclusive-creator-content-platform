b8f6aef5e92ef4a41656589bf76b5481061755fb
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-23 04:21:17 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -79,7 +79,8 @@ function updateWranglerJsonc() {
 function runSetupCommands() {
     const commands = [
     "bun add tailwindcss@^3.4.7",
-    "bun add react-intersection-observer@^9.4.0"
+    "bun add react-intersection-observer@^9.4.0",
+    "bun install @hono/zod-validator"
 ];
     
     if (commands.length === 0) {
